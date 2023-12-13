ff2e6e5fee78af91003c15baad86a918c3de0c6c
yeahimjt <80553412+yeahimjt@users.noreply.github.com>
2023-12-13 00:15:37 -0600

    updating component

--- app/components/skills/icon.tsx
+++ app/components/skills/icon.tsx
@@ -1,45 +1,8 @@
 'use client';
 import React, { useEffect } from 'react';
 
-// import ReactIcon from './icons/react.svg';
-// import NextIcon from './icons/next.svg';
-// import TailwindIcon from './icons/tailwind.svg';
-// import JavaScriptIcon from './icons/javascript.svg';
-// import TypeScriptIcon from './icons/typescript.svg';
-// import FirebaseIcon from './icons/firebase.svg';
-// import ExpressIcon from './icons/express.svg';
-// import AwsIcon from './icons/aws.svg';
-// import AzureIcon from './icons/azure.svg';
-// import MySQLIcon from './icons/mysql.svg';
-// import MongoIcon from './icons/mongo.svg';
-// import JavaIcon from './icons/java.svg';
-// import CIcon from './icons/c.svg';
-// import PythonIcon from './icons/python.svg';
-// import { handleSkillContainer } from '@/app/animations';
 import Image from 'next/image';
 import { handleSkillContainer } from '@/app/animations';
-
-// interface IconTypes {
-//   [key: string]: string;
-// }
-
-// const iconTypes: IconTypes = {
-//   react: ReactIcon,
-//   next: NextIcon,
-//   tailwind: TailwindIcon,
-//   javascript: JavaScriptIcon,
-//   typescript: TypeScriptIcon,
-//   firebase: FirebaseIcon,
-//   express: ExpressIcon,
-//   aws: AwsIcon,
-//   azure: AzureIcon,
-//   mysql: MySQLIcon,
-//   mongo: MongoIcon,
-//   java: JavaIcon,
-//   c: CIcon,
-//   csharp: CSharpIcon,
-//   python: PythonIcon,
-// };
 
 interface IconComponentProps {
   name: string;
