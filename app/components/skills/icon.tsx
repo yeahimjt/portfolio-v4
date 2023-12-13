087636a9ef07ef3c66f9f02d429869a393c8b93e
yeahimjt <80553412+yeahimjt@users.noreply.github.com>
2023-12-13 00:26:22 -0600

    updating again

--- app/components/skills/icon.tsx
+++ app/components/skills/icon.tsx
@@ -1,28 +1,60 @@
 'use client';
 import React, { useEffect } from 'react';
 
-import Image from 'next/image';
+import ReactIcon from '@/public/icons/react.svg';
+import NextIcon from '@/public/icons/next.svg';
+import TailwindIcon from '@/public/icons/tailwind.svg';
+import JavaScriptIcon from '@/public/icons/javascript.svg';
+import TypeScriptIcon from '@/public/icons/typescript.svg';
+import FirebaseIcon from '@/public/icons/firebase.svg';
+import ExpressIcon from '@/public/icons/express.svg';
+import AwsIcon from '@/public/icons/aws.svg';
+import AzureIcon from '@/public/icons/azure.svg';
+import MySQLIcon from '@/public/icons/mysql.svg';
+import MongoIcon from '@/public/icons/mongo.svg';
+import JavaIcon from '@/public/icons/java.svg';
+import CIcon from '@/public/icons/c.svg';
+import CSharpIcon from '@/public/icons/csharp.svg';
+import PythonIcon from '@/public/icons/python.svg';
 import { handleSkillContainer } from '@/app/animations';
+
+interface IconTypes {
+  [key: string]: string;
+}
+
+const iconTypes: IconTypes = {
+  react: ReactIcon,
+  next: NextIcon,
+  tailwind: TailwindIcon,
+  javascript: JavaScriptIcon,
+  typescript: TypeScriptIcon,
+  firebase: FirebaseIcon,
+  express: ExpressIcon,
+  aws: AwsIcon,
+  azure: AzureIcon,
+  mysql: MySQLIcon,
+  mongo: MongoIcon,
+  java: JavaIcon,
+  c: CIcon,
+  csharp: CSharpIcon,
+  python: PythonIcon,
+};
 
 interface IconComponentProps {
   name: string;
 }
 
 const IconComponent = ({ name }: IconComponentProps) => {
+  let Icon = iconTypes[name];
   useEffect(() => {
     handleSkillContainer();
   }, []);
   return (
     <div className='skill-container group relative bg-light-container dark:bg-dark-container'>
       <div className='opacity-100 transition-opacity group-hover:opacity-0'>
-        <Image
-          src={require(`@/public/icons/${name}.svg`)}
-          width={95}
-          height={95}
-          alt=''
-        />
+        <Icon />
       </div>
-      <p className='absolute bottom-1 right-1 text-light-title opacity-0 transition-opacity group-hover:opacity-100 dark:text-dark-title'>
+      <p className='absolute bottom-1 right-1 text-light-title opacity-0 group-hover:opacity-0 dark:text-dark-title'>
         {name}
       </p>
     </div>
