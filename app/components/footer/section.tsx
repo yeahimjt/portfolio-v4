c75e6cc3bf7d02b4365939009d152227eb6579fa
Jonathan Trevino <jonathantrevino@Jonathans-MBP.T-mobile.com>
2024-02-10 20:05:57 -0600

    Adding space from edge

--- app/components/footer/section.tsx
+++ app/components/footer/section.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const FooterSection = () => {
   return (
-    <footer className='col-span-6 flex h-[60px] flex-col justify-end pt-2 dark:border-dark-container md:flex-row'>
+    <footer className='col-span-6 flex h-[60px] flex-col justify-end pb-4 pr-4 dark:border-dark-container md:flex-row'>
       <h5 className='text-end text-[10px] text-light-general dark:text-dark-general md:text-[14px]'>
         Designed and developed by Jonathan Trevino
       </h5>
